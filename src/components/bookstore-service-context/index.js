3e73faa72cf4ce017766067085a84131546b31e1
Evgenii <freeedayy@gmail.com>
2021-11-09 21:11:34 +0300

    redux started

---
+++ src/components/bookstore-service-context/index.js
@@ -0,0 +1,7 @@
+import {BookstoreServiceConsumer} from "./bookstore-servive-context";
+import {BookstoreServiceProvider} from "./bookstore-servive-context";
+
+export {
+    BookstoreServiceConsumer,
+    BookstoreServiceProvider
+}
